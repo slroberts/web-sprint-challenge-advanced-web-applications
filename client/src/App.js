27ab9c5c214da5f29b4fe2427c3f3965c1211124
slroberts <shomariroberts@gmail.comclear>
2020-08-17 00:05:06 -0400

    added Switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
@@ -15,7 +15,9 @@ function App() {
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
-        <PrivateRoute exact path="/bubbles" component={BubblePage} />
+        <Switch>
+          <PrivateRoute exact path="/bubbles" component={BubblePage} />
+        </Switch>
       </div>
     </Router>
   );
