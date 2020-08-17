31d430dc7c1fe4662ff26ce90f2865a47be0e4ae
slroberts <shomariroberts@gmail.comclear>
2020-08-16 21:52:06 -0400

    completed BubblePage component

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, {useState, useEffect} from "react";
+import {axiosWithAuth} from "../utils/axiosWithAuth";
+import {useHistory} from "react-router-dom";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
@@ -9,8 +10,27 @@ const BubblePage = () => {
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
+  const history = useHistory();
+
+  const getColorList = () => {
+    axiosWithAuth()
+      .get("/api/colors")
+      .then((res) => setColorList(res.data))
+      .catch((err) => console.log(err.response));
+  };
+
+  useEffect(() => {
+    getColorList();
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.clear();
+    history.push("/");
+  };
+
   return (
     <>
+      <button onClick={handleLogout}>Logout</button>
       <ColorList colors={colorList} updateColors={setColorList} />
       <Bubbles colors={colorList} />
     </>
