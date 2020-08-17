abce80f03690b114b97bc2de3c05bee19451e0ff
slroberts <shomariroberts@gmail.comclear>
2020-08-16 23:58:32 -0400

    changed position of add color form

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -75,63 +75,7 @@ const ColorList = ({colors, updateColors, setDependency}) => {
   };
 
   return (
-    <div className="colors-wrap">
-      <p>colors</p>
-      <ul>
-        {colors.map((color) => (
-          <li key={color.color} onClick={() => editColor(color)}>
-            <span>
-              <span
-                className="delete"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  deleteColor(color);
-                }}
-              >
-                x
-              </span>{" "}
-              {color.color}
-            </span>
-            <div
-              className="color-box"
-              style={{backgroundColor: color.code.hex}}
-            />
-          </li>
-        ))}
-      </ul>
-      {editing && (
-        <form onSubmit={saveEdit}>
-          <legend>edit color</legend>
-          <label>
-            color name:
-            <input
-              onChange={(e) =>
-                setColorToEdit({...colorToEdit, color: e.target.value})
-              }
-              value={colorToEdit.color}
-            />
-          </label>
-          <label>
-            hex code:
-            <input
-              onChange={(e) =>
-                setColorToEdit({
-                  ...colorToEdit,
-                  code: {hex: e.target.value},
-                })
-              }
-              value={colorToEdit.code.hex}
-            />
-          </label>
-          <div className="button-row">
-            <button type="submit">save</button>
-            <button onClick={() => setEditing(false)}>cancel</button>
-          </div>
-        </form>
-      )}
-      <div className="spacer" />
-      {/* stretch - build another form here to add a color */}
-
+    <>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
@@ -154,7 +98,64 @@ const ColorList = ({colors, updateColors, setDependency}) => {
         />
         <button>Add Color</button>
       </form>
-    </div>
+      <div className="colors-wrap">
+        <p>colors</p>
+        <ul>
+          {colors.map((color) => (
+            <li key={color.color} onClick={() => editColor(color)}>
+              <span>
+                <span
+                  className="delete"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    deleteColor(color);
+                  }}
+                >
+                  x
+                </span>{" "}
+                {color.color}
+              </span>
+              <div
+                className="color-box"
+                style={{backgroundColor: color.code.hex}}
+              />
+            </li>
+          ))}
+        </ul>
+        {editing && (
+          <form onSubmit={saveEdit}>
+            <legend>edit color</legend>
+            <label>
+              color name:
+              <input
+                onChange={(e) =>
+                  setColorToEdit({...colorToEdit, color: e.target.value})
+                }
+                value={colorToEdit.color}
+              />
+            </label>
+            <label>
+              hex code:
+              <input
+                onChange={(e) =>
+                  setColorToEdit({
+                    ...colorToEdit,
+                    code: {hex: e.target.value},
+                  })
+                }
+                value={colorToEdit.code.hex}
+              />
+            </label>
+            <div className="button-row">
+              <button type="submit">save</button>
+              <button onClick={() => setEditing(false)}>cancel</button>
+            </div>
+          </form>
+        )}
+        <div className="spacer" />
+        {/* stretch - build another form here to add a color */}
+      </div>
+    </>
   );
 };
 
